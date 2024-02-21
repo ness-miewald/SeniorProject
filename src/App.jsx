f3820a27be48411fe9a0c85805d08e47e08d737e
ness-miewald <83977281+ness-miewald@users.noreply.github.com>
2024-02-20 19:24:00 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './css/App.css'
+import Footer from './Footer';
+
+function App() {
+  return (
+      <>
+        <h1>Aim Plus Medical Supplies Portal</h1>
+        <Footer />
+      </>
+  )
+}
+
+export default App
